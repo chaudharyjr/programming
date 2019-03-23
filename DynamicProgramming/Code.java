d494f3e4a656f3bc463846d062dcb9cc0b91fe85
Shubham Chaudhary <shubham03@outlook.com>
2019-03-23 19:30:46 +0530

    Uploaded Code.java

---
+++ DynamicProgramming/Code.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Code{
+	public static void main(String [] Args){
+		Scanner scan=new Scanner(System.in);
+	//	int m=scan.nextInt();
+		System.out.println("Hey");
+	}	
+}
+//https://github.com/chaudharyjr/CodeforcesJava.git
