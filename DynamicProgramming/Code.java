6c9c5f5fc9b6b4c9638d922c6b3c426be8bc6a66
Shubham Chaudhary <shubham03@outlook.com>
2019-03-23 21:19:27 +0530

    Deleted Empty folder

--- DynamicProgramming/Code.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class Code{
-	public static void main(String [] Args){
-		Scanner scan=new Scanner(System.in);
-	//	int m=scan.nextInt();
-		System.out.println("Hey");
-	}	
-}
-//https://github.com/chaudharyjr/CodeforcesJava.git
