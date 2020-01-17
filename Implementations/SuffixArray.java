667c58c2fd5d82771601018746fd538c0b23ab83
Shubham Chaudhary <48656157+chaudharyjr@users.noreply.github.com>
2020-01-17 22:42:45 +0530

    Updated SuffixArray.java

--- Implementations/SuffixArray.java
+++ Implementations/SuffixArray.java
@@ -5,19 +5,25 @@ import java.io.*;
 /* Name of the class has to be "Main" only if the class is public. */
 class SuffixArray
 {
+    public static int charToIndex(char x) {
+	if(x == '$')
+	   return 0;
+	return x-'a'+1;
+    }
+		
     public static int [] sortCharacters(String s){
     	char[] arr=s.toCharArray();
-    	int [] count=new int[26];
+    	int [] count=new int[27];
     	int [] order=new int[s.length()];
     	for(char c:arr){
-    		count[c-'a']++;
+    		count[charToIndex(c)]++;
     	}
-    	for(int i=1;i<s.length();i++){
+    	for(int i=1;i<count.length();i++){
     		count[i]+=count[i-1];
     	}
     	for(int i=s.length()-1;i>=0;i--){
-    		count[arr[i]-'a']-=1;
-    		order[count[arr[i]-'a']]=i;
+    		count[charToIndex(arr[i])]-=1;
+    		order[count[charToIndex(arr[i])]]=i;
     	}
     	return order;
     }
@@ -35,23 +41,42 @@ class SuffixArray
     	}
     	return classes;
     }
+    
+     public static int [] sortDoubled(String s,int l,int [] order,int [] class){
+     		int [] newOrder=new int[s.length()];
+		    int [] count=new int[s.length()];
+		    for(int i=0;i<class.length();i++){
+			    count[class[i]]+=1;
+		    }
+	    	for(int i=1;i<s.length();i++){
+	    		count[class[i]]+=count[class[i-1]];
+	    	}
+	    	for(int i=s.length()-1;i>=0;i--){
+	    		int start=(order[i]-l+s.length())%s.length();
+	    		int cl=class[start];
+	    		count[cl]-=1;
+	    		newOrder[count[cl]]=start;
+	    	}
+	    	return newOrder;	
+    }
 
-    public static int [] sortDoubled(String s,int l,int [] order,int [] class){
-	    int [] newOrder=new int[s.length()];
-	    int [] count=new int[s.length()];
-	    for(int i=0;i<class.length();i++){
-		    count[class[i]]+=1;
-	    }
-    	for(int i=1;i<s.length();i++){
-    		count[class[i]]+=count[class[i-1]];
+
+    public static int [] updateClass(int n,int l,int [] newOrder,int [] class_old){
+	int [] newClass=new int[n];
+	int [] count=new int[n];
+	newClass[newOrder[0]]=0;
+    	for(int i=1;i<n;i++){
+    		int curr=newOrder[i];
+    		int prev=newOrder[i-1];
+    		int curr_mid=(newOrder[i]+l)%newOrder.length();
+    		int prev_mid=(newOrder[i-1]+l)%newOrder.length();
+    		if(class_old[curr]!=class_old[prev] || class_old[curr_mid]!=class_old[prev_mid]){
+    			newClass[curr]=newClass[prev]+1;
+    		}else{
+    			newClass[curr]=newClass[prev];
+    		}
     	}
-    	for(int i=s.length()-1;i>=0;i--){
-    		int start=(order[i]-l+s.length())%s.length();
-    		int cl=class[start];
-    		count[cl]-=1;
-    		newOrder[count[cl]]=start;
-    	}
-    	return newOrder;
+    	return newClass;
     }
 
     
